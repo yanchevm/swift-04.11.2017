53465c8d8982790fa337950a23db70ae7c05094b
Ivaylo-I <iliev_blitzschutz@abv.bg>
2017-11-12 14:22:03 +0200

    test

---
+++ Students/Ivaylo Iliev/CalculateExpression.java
@@ -0,0 +1,17 @@
+package HW_20171108;
+
+	/**
+	 * Calculation program.
+	 */
+public class CalculateExpression {
+
+	public static void main(String[] args) {
+		double result1 = (3291 + 88581) / 14;
+		double result2 = (1116 % 171) * 5;
+		double result3 = 312 / (4 + 18);
+		
+		System.out.println(result1 + result2 + result3);
+		
+	}
+
+}
