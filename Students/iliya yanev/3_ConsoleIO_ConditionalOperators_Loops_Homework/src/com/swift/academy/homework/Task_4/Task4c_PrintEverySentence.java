1316a26ad04af9b6933df5d14dd344910c3a7279
iliyaYanev <iliya.yanev@mentormate.com>
2017-11-14 05:19:52 +0200

    Added task 4

--- Students/iliya yanev/3_ConsoleIO_ConditionalOperators_Loops_Homework/src/com/swift/academy/homework/Task_4/Task4c_PrintEverySentence.java
+++ Students/iliya yanev/3_ConsoleIO_ConditionalOperators_Loops_Homework/src/com/swift/academy/homework/Task_4/Task4c_PrintEverySentence.java
@@ -2,12 +2,14 @@ package com.swift.academy.homework.Task_4;
 
 import java.util.Scanner;
 
-public class Task4a_StringVowels {
+public class Task4c_PrintEverySentence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("String: ");
-        String str = sc.nextLine().replaceAll("[AEIOUaeiou]", "");
-        System.out.println(str);
+        String[] sentences = sc.nextLine().trim().split("(?<=[.!?])\\s");
+        for (String s:sentences) {
+            System.out.println(s);
+        }
     }
 }
