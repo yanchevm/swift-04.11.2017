ff5f74b5ed586a9c08600c5dda4a96df9d70cea7
Iliya Yanev <iliya.yanev@mentormate.com>
2017-11-16 13:55:04 +0200

    Added task 1

---
+++ Students/iliya yanev/4_ArraysStringsMemorySourceControlHomerowk/src/com/swift/academy/homework/Task_1/Task1b_PrintMatrix.java
@@ -0,0 +1,28 @@
+package com.swift.academy.homework.Task_1;
+
+import com.swift.academy.homework.Task_0.Task0b_ReadArray;
+
+public class Task1b_PrintMatrix {
+    public static void main(String[] args) {
+        System.out.print("Enter array size: ");
+        int size = Task0b_ReadArray.validateNumber();
+        while (size < 1) {
+            System.out.print("Please enter a positive number: ");
+            size = Task0b_ReadArray.validateNumber();
+        }
+        int arr[][] = new int[size][size];
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr[1].length; j++) {
+                    if (j % 2 == 0) {
+                        arr[i][j] = 1 + i + j * size;
+                        System.out.format("%4d", arr[i][j]);
+                    }
+                    else {
+                        arr[i][j] =  size * (j + 1) - i;
+                        System.out.format("%4d", arr[i][j]);
+                    }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Students/iliya yanev/4_ArraysStringsMemorySourceControlHomerowk/src/com/swift/academy/homework/Task_1/Task1d_PrintMatrix.java
@@ -0,0 +1,48 @@
+package com.swift.academy.homework.Task_1;
+
+import com.swift.academy.homework.Task_0.Task0b_ReadArray;
+
+public class Task1d_PrintMatrix {
+    public static void main(String[] args) {
+        System.out.print("Enter array size: ");
+        int size = Task0b_ReadArray.validateNumber();
+        while (size < 1) {
+            System.out.print("Please enter a positive number: ");
+            size = Task0b_ReadArray.validateNumber();
+        }
+
+        int arr[][] = spiral(size);
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                System.out.format("%4d", arr[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static int[][] spiral(int size) {
+        int[][] arr = new int[size][size];
+        int insideCells = (int)Math.ceil((size / 2.0));
+        int sizeIndex = size;
+        int count = 1;
+
+        for (int i = 0; i < insideCells; i++) {
+            for (int j = 0; j < sizeIndex; j++) {
+                arr[i][i+j] = count++;
+            }
+            for (int j = 1; j < sizeIndex; j++) {
+                arr[i + j][size - 1 - i] = count++;
+            }
+            for (int j = sizeIndex - 2; j >= 0; j--) {
+                arr[size - 1 - i][i + j] = count++;
+            }
+            for (int j = sizeIndex -2; j > 0; j--) {
+                arr[i + j][i] = count++;
+            }
+            sizeIndex -= 2;
+        }
+        return arr;
+    }
+}
