d279a0437a16e0ad79b4d8ad1e32afc99ff3c87b
RazorSharpBG <kiril.tenchev1@gmail.com>
2017-11-16 17:38:56 +0200

    Signed-off-by: RazorSharpBG <kiril.tenchev1@gmail.com>

---
+++ Students/Kiril Tenchev/src/Homework/HWLecture3/Task1b.java
@@ -0,0 +1,17 @@
+package Homework.HWLecture3;
+import java.util.Scanner;
+public class Task1b {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num, reverse = 0;
+        System.out.print("Enter the number to reverse: ");
+        num = sc.nextInt();
+        while( num != 0 )
+        {
+            reverse *=  10;
+            reverse +=  num%10;
+            num /=10;
+        }
+        System.out.println("Reverse of entered number is "+reverse);
+    }
+}
---
+++ Students/Kiril Tenchev/src/Homework/HWLecture3/Task2b.java
@@ -0,0 +1,20 @@
+package Homework.HWLecture3;
+import java.util.Scanner;
+public class Task2b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers do you want: ");
+        int size=Integer.parseInt(sc.nextLine());
+        String str=sc.nextLine();
+        String []nums=str.split(" ");
+        if(nums.length!=size){
+            System.out.println("Illegal number of elements");
+        }
+        else{
+            for (int i = nums.length-1; i >=0 ; i--) {
+                System.out.print(nums[i] + " ");
+            }
+        }
+
+    }
+}
