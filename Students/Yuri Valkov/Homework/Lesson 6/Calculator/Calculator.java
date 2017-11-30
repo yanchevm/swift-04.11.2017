8706b06f6f61a1083ba1a7b55f242c8d69b7725f
Tribalmixes <iurikid@gmail.com>
2017-11-30 12:08:53 +0200

    new

---
+++ Students/Yuri Valkov/Homework/Lesson 6/Calculator/Calculator.java
@@ -0,0 +1,27 @@
+package Credentials.Calculator;
+
+public class Calculator {
+
+
+    public double subtract(double a, double b){
+        return a-b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+
+    public double sum(double a, double b){
+        return a+b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+
+
+
+    public double percentage(double a, double b){
+        return (b/a)*100;
+    }
+}
---
+++ Students/Yuri Valkov/Homework/Lesson 6/Credentials/Task2_CredentialsManager.java
@@ -0,0 +1,55 @@
+package Credentials;
+import java.util.Scanner;
+
+public class Task2_CredentialsManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Credentials[] cr = new Credentials[200];
+        int index = 0;
+
+        System.out.println("Please, enter command, username, pass and new pass if you want to change it: ");
+        String input = sc.nextLine();
+
+        while(!input.equals("END")){
+            String[] parts = input.split(" ");
+
+            if(parts[0].equals("ENROLL")){
+                cr[index] = new Credentials(parts[1],parts[2]);
+                index++;
+                System.out.println("ENROLL success");
+            }
+            if(parts[0].equals("CHPASS")){
+                for (int i = 0; i < cr.length; i++) {
+                    if(parts[1].equals(cr[i].getUsername())){
+                        cr[i].setPassword(parts[2],parts[3]);
+                        if (!cr[i].getWrongPass2()){
+                            System.out.println("CHPASS success");
+                        }
+                        else {
+                            System.out.println("CHPASS fail");
+                        }
+                        break;
+                    }
+                }
+            }
+            if(parts[0].equals("AUTH")){
+                for (int i = 0; i < cr.length; i++) {
+                    if(parts[1].equals(cr[i].getUsername())){
+                        if (cr[i].checkPassword(parts[2])){
+                            System.out.println("AUTH success");
+                        }
+                        else {
+                            System.out.println("AUTH fail");
+                        }
+                        break;
+                    }
+                }
+            }
+
+            System.out.println("Please, enter command, username, pass and new pass if you want to change it:");
+            input = sc.nextLine();
+        }
+
+    }
+}
