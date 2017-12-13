c82c138a66d3b64544fb34c24b701c6a8e9bd2c1
Gloriya Hristova <gloriahristova@yahoo.co.uk>
2017-12-13 22:05:10 +0200

    //

--- Students/Gloriya Hristova/L4/Task3e_ConvertibleStrings.java
+++ Students/Gloriya Hristova/L4/Task3e_ConvertibleStrings.java
@@ -5,28 +5,46 @@ import java.util.Scanner;
 
 public class Task3e_ConvertibleStrings {
       
-  public static void main(String [] args ){
-      Scanner input = new Scanner (System.in);
-      String text, str,line="coool"; 
-      text = input.nextLine();
-      str = input.nextLine();
-      /*if(text.contains(str)){
-          System.out.println(true);
-      }else {
-          System.out.println(false);
-      }*/
-      //String num = input.nextLine();
-       //System.out.println("Mirror number:");
-       for(int j=0;j<=str.length()-1;j++){
-          //System.out.print( num.charAt(j));
-          for(int k =0;k<= text.length()-1; k++){
-             if(str.charAt(j)==text.charAt(k)){
-                // text.replace(text.charAt(k), "");
-             }
-       }
+   public static void main(String [] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        boolean flag = false;
+        int len1,len2;
+        String text1,text2,temp;
+        StringBuilder s = new StringBuilder();
+        char symbol;
        
-  
-       }
-      
-  }
+        
+        System.out.println("Enter strings: ");
+        text1 = input.nextLine();
+        text2 = input.nextLine();
+        len1 = text1.length();
+        len2 = text2.length();
+        
+        if(len2>len1){
+            temp=text2;
+            text2=text1;
+            text1=temp;
+        }
+        
+        for(int j = 0; j <len2; j++){
+           
+            for(int q = 0; q <len1; q++){
+                
+                symbol=text2.charAt(j);
+                s=s.append(symbol);
+                flag = text1.contains(s);
+                
+                if(flag == false){
+                    System.out.println(flag);
+                    System.exit(0);
+                }
+                s.setLength(0);
+            }
+        } 
+        if(flag==true){
+            System.out.println(flag);
+        }
+   }
 }
