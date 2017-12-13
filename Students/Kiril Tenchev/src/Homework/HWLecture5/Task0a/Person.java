f36fb00f0dec153a7e7d852fd5a71dc0eb44c06b
RazorSharpBG <kiril.tenchev1@gmail.com>
2017-12-13 19:12:17 +0200

    ?

---
+++ Students/Kiril Tenchev/src/Homework/HWLecture5/Task0a/Person.java
@@ -0,0 +1,63 @@
+package Homework.HWLecture5.Task0a;
+
+public class Person {
+    private String name;
+    private int years;
+
+    public Person() {
+        name = "No name";
+        years = -1;
+    }
+
+    public Person(String nameValue) {
+        name = nameValue;
+        years = -1;
+    }
+
+    public Person(String nameValue, int yearsValue) {
+        name=nameValue;
+        years=yearsValue;
+    }
+
+    public void print() {
+        if((!name.equals("No name")&&!name.equals(""))&&years<0) {
+            System.out.println("Hello, I'm " + name + ".");
+        }
+        else if ((name.equals("No name")||name.equals(""))&&years<0) {
+            System.out.println("I'm John Doe.");
+        }
+         else if ((!name.equals("No name")&&!name.isEmpty())&&years>=0) {
+            System.out.println("Hello, I'm " + name + ". I'm " + years + " years old.");
+        }
+    }
+
+
+
+    public void setName(String nameValue) {
+        if (!name.matches("[A-Za-z]")&&!name.equals("No name")){
+            System.err.println("Incorrect data.");
+        }
+        else
+           this.name = nameValue;
+    }
+
+
+
+    public void setYears(int yearsValue) {
+       if(years>=0||years==-1){
+           this.years = yearsValue;
+       }
+       else{
+           System.out.println("Illegal data.");
+       }
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getYears() {
+        return this.years;
+    }
+}
---
+++ Students/Kiril Tenchev/src/Homework/HWLecture5/Task0a/main.java
@@ -0,0 +1,18 @@
+package Homework.HWLecture5.Task0a;
+
+public class main {
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.print();
+        System.out.println("........................................");
+        Person p2=new Person("Kiril");
+        p2.print();
+        System.out.println("........................................");
+        Person p3=new Person("Ivan",15);
+        p3.print();
+        System.out.println("........................................");
+
+
+
+    }
+}
