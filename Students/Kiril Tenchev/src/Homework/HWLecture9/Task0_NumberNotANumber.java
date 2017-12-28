a4823b22bd8fed114a2400a9561ffa35ee4e644a
RazorSharpBG <kiril.tenchev1@gmail.com>
2017-12-28 22:57:11 +0200

    Howework from lecture 9 and 6 (Calculator).

---
+++ Students/Kiril Tenchev/src/Homework/HWLecture9/Task0_NumberNotANumber.java
@@ -0,0 +1,31 @@
+package Homework.HWLecture9;
+import java.util.Scanner;
+public class Task0_NumberNotANumber {
+
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner (System.in);
+        String number = "";
+        System.out.println("Enter number: ");
+
+        while (!number.equals("END")){
+            number = sc.nextLine();
+
+            if(number.equals("END")){return;}
+
+            try {
+                Integer.parseInt(number);
+                System.out.println(BLUE + "Number." + RESET);
+                System.out.println("Enter number: ");
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Enter number: ");
+                System.err.println("Not a number.");
+            }
+        }//когато разделя със спейс изкарва съобщението преди съобщението за грешка
+        // и не знам как да го оправя
+    }
+}
